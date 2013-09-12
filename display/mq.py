644b183f6c21eee8288272dbbed3c705223b2c8a
Joe Ally <sleek.joe@gmail.com>
2013-09-12 14:41:30 +0100

    ADD: Added a python script to test RabbitMQ

---
+++ display/mq.py
@@ -0,0 +1,17 @@
+import pika
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(
+	        host='localhost'))
+channel = connection.channel()
+
+channel.queue_declare(queue='screendisplay')
+
+print ' [*] Waiting for messages. To exit press CTRL+C'
+
+lcd_scroller =  LCDLinearScroll(())
+def callback(ch, method, properties, body):
+	lcd_scroller.display_message(body)
+
+
+channel.basic_consume(callback, queue='screendisplay', no_ack=True)
+channel.start_consuming()
