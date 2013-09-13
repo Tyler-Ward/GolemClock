392675cf1f7f50cb44da04dba3cdb0771655424c
Joe Ally <sleek.joe@gmail.com>
2013-09-13 16:33:45 +0100

    FIX: Now using popen to start and stop sounds

--- audio/AlarmSound.py
+++ audio/AlarmSound.py
@@ -1,25 +1,25 @@
-from subprocess import call
+from subprocess import Popen
 from threading import Timer
 from time import sleep
 
 class AlarmSound:
 	def __init__(self, delay=1):
 		self.delay = delay
+		self.started = False
 
 	def start_sound(self, url):
-		def callback():
-			call(["omxplayer", url])
-			self.timer = Timer(self.delay, callback)
-			self.timer.start()
 		callback()
 
 	def play(self, url):
-		self.start_sound(url)
+		if not self.started:
+			self.process = Popen(["mplayer", url, "-loop", "0"])
+			self.started = True
 
 	def stop(self):
 		print("canceling")
-		self.timer.cancel()
+		self.process.kill()
+		self.started = False
 
-alarm_sound = AlarmSound(0.5)
+#alarm_sound = AlarmSound(0.5)
 #alarm_sound.play("sound.wav")
 #alarm_sound.stop()
