aac55080e7675309edcebf7e02e4a3da75e9958c
Joe Ally <sleek.joe@gmail.com>
2013-09-13 11:40:14 +0100

    ADD: added a function to weather create a result from weather data

--- weather/weather.py
+++ weather/weather.py
@@ -7,4 +7,14 @@ def get_weather(city, country):
 	response = urllib2.urlopen(req)
 	return json.loads(response.read())
 
-print( get_weather("Southampton", "UK") )
+
+def weather_message(city, country):
+	obj = get_weather(city, country)
+	msgs = []
+	if obj['weather'][0]['main'] == u'Rain':
+		msgs.append("Chance of rain")
+	if obj['weather'][0]['main'] ==  u'Snow':
+		msgs.append("Chance of snow")	
+	return {'type': 'weather', 'messages': msgs, 'possible_delays': True}
+
+print( weather_message("Southampton", "UK") )
